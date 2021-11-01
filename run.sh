ac16248c6458d3b109f8e344f5d4bc2401a315e4
Diamantis Sellis <sellisd@gmail.com>
2021-11-01 21:04:41 +0000

    fix. Correct installation and paths

--- run.sh
+++ run.sh
@@ -2,13 +2,14 @@ pyenv install 3.9.0
 pyenv local 3.9.0
 git clone https://github.com/sellisd/gitrepodb.git
 cd gitrepodb
-python3 -m pip install -e .
+python3 -m pip install .
+cd ..
+python3 -m pip install -r requirements.txt
 mkdir data
 # Get repositories and Run analysis
 gitrepodb init --name ./repositories.db --overwrite \
     && gitrepodb query --project jupyter --head 100 \
     && gitrepodb add --basepath ./data \
-    && gitrepodb download --project jupyter \
-    && pycodeseq --input_path ./data --output jupyter.tsv --method cells
+    && gitrepodb download --project jupyter
 
-python ./jupyter_language.py
+python ./jupyter_language.py ./gitrepodb/data
